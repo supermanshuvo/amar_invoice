8cf1260d2d3a10b88cc98b15b3a4e1acf6521fe0
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-11 16:13:57 +0600

    Create seed and factory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'parent_id', 'featured', 'menu', 'image'
+    ];
+
+    protected $casts = [
+        'parent_id' =>  'integer',
+        'featured'  =>  'boolean',
+        'menu'      =>  'boolean'
+    ];
 }
