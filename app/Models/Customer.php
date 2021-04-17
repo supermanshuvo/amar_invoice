1c3cad51ebde0bc08772d228d167dafa62cb0c5a
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-18 00:09:10 +0600

    Add one to one reletiong in customer models

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+    protected $table = 'customers';
+    protected $fillable = [
+        'customer_name',
+        'customer_contact',
+        'customer_alt_contact',
+        'customer_address',
+    ];
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
 }
