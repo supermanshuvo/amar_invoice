240c6a81b726b0a650fa2873e64dd4a408c62186
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-18 00:34:34 +0600

    set the proudct table for add product and make relation in invoice

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
+    protected $fillable = [
+        'product_name',
+        'product_company_name',
+        'product_details',
+        'product_type',
+        'product_price',
+        'product_image',
+    ];
+    public function invoice(){
+        return $this->hasOne('App\Models\Invoice');
+    }
 }
