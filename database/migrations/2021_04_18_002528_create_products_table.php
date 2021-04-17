994b42b0ebf0ff8f8045b7283e99412bd73e442b
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-18 00:29:27 +0600

    Create products table and colums for add products

---
+++ database/migrations/2021_04_18_002528_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('product_company_name');
+            $table->text('product_details');
+            $table->string('product_price');
+            $table->string('product_type');
+            $table->string('product_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
