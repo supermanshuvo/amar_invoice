55c5120e624d5f7998d7335485472f9cdca92b19
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-18 23:32:56 +0600

    Add customer id in invoice table

--- database/migrations/2021_04_18_003510_create_invoices_table.php
+++ database/migrations/2021_04_18_003510_create_invoices_table.php
@@ -16,13 +16,11 @@ class CreateInvoicesTable extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('customer_id')->unsigned();
-            $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('product_id')->unsigned();
             $table->string('product_quantity');
             $table->string('Order_id');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('customers')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
