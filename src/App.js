43224298d37730b24f24a3470291aeb4f1023ffa
Rezaul Karim <shuvothesuperman@gmail.com>
2021-09-13 17:14:47 +0600

    Add material ui :

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Button from '@material-ui/core/Button';
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Amar Bipony</h1>
+      <p>Shuv is the best in the world</p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
